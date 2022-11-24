0e6d0b7ccb85efff89404423a5152ae5d5df0815
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-11-24 10:02:39 +0100

    update

--- src/main/java/DAO/ChauffeurDAO.java
+++ src/main/java/DAO/ChauffeurDAO.java
@@ -2,18 +2,37 @@ package DAO;
 
 import Services.JPA;
 import entities.Chauffeur;
-import entities.Staff;
 import jakarta.persistence.Query;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class ChauffeurDAO {
-    public static void addChauffeur(Chauffeur chauffeur) {
+public class ChauffeurDAO implements DAO <Chauffeur>{
+    
+
+    @Override
+    public Chauffeur get(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Chauffeur> getAll() {
+        Query query = JPA.entityManager().createQuery("SELECT c from Chauffeur c");
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Chauffeur chauffeur) {
         JPA.serv(em -> em.persist(chauffeur));
     }
 
-    public static List<Chauffeur> getAllChauffeur() {
-        Query query = JPA.entityManager().createQuery("SELECT c from Chauffeur c");
-        return query.getResultList();
+    @Override
+    public void update(Chauffeur chauffeur) {
+
+    }
+
+    @Override
+    public void delete(long id) throws SQLException {
+
     }
 }
