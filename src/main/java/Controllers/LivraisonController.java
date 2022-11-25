2835de79457e525ed685c821304363182524dc71
abdelghafour-ettaqui <ettaqui88@gmail.com>
2022-11-25 12:06:31 +0100

    get trajet

--- src/main/java/Controllers/LivraisonController.java
+++ src/main/java/Controllers/LivraisonController.java
@@ -1,2 +1,51 @@
-package Controllers;public class LivraisonController {
+package Controllers;
+
+
+import DAO.LivraisonDAO;
+import Services.Enum;
+import entities.Livraison;
+import jakarta.annotation.PostConstruct;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+
+public class LivraisonController {
+
+    @PostConstruct
+    public void initialize(){
+        this.vehicleCategories = vehicleCatDao.getAll();
+        this.deliveries = deliveryDao.getAll();
+    }
+
+    public static boolean addLivraison(double poid, double prix, Integer idCh, int trajet){
+        Livraison livraison = new Livraison();
+        livraison.setStatus(String.valueOf(Enum.statusVal.en_attente));
+        livraison.setPoid(poid);
+        livraison.setPrix(prix);
+        livraison.setTrajet_id(trajet);
+        livraison.setDate(Date.valueOf(LocalDate.now()));
+        livraison.setChauffeur_id(idCh);
+
+
+        LivraisonDAO livraisonDAO = new LivraisonDAO();
+        livraisonDAO.save(livraison);
+        return true;
+    }
+
+    public static boolean LivraisonStatusDelivre(int id){
+        if ( LivraisonDAO.UpdateLivraisonStatusById(String.valueOf(Enum.statusVal.delivré), id)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean LivraisonStatusEncour(int id){
+        if ( LivraisonDAO.UpdateLivraisonStatusById(String.valueOf(Enum.statusVal.en_attente), id)){
+            return true;
+        }
+        return false;
+    }
+
+
 }
